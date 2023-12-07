88327483382d02463b62236e4c429eb82a3ebca7
Your Name <bimochanniraula08@gmail.com>
2023-12-07 10:55:12 +0545

    remaining
    Signed-off-by: Your Name <bimochanniraula08@gmail.com>

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./ImageGenerator.css";
 import default_image from "../Assets/default_image.svg";
 const ImageGenerator = () => {
+  const [image_url, setImage_url] = useState("/");
+  let inputRef = useRef(null);
+  const imageGenerator = async () => {
+    if (inputRef.current.value === "") {
+      return 0;
+    }
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+            "Bearer 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",
+          "User-Agent": "Chrome",
+        },
+        body: JSON.stringify({
+          prompt: `${inputRef.current.value}`,
+          n: 1,
+          size: "512*512",
+        }),
+      }
+    );
+    let data = await response.json();
+    let data_array = data.data;
+    setImage_url(data_array[0].image_url);
+  };
   return (
     <div className="ai-image-generator">
       <div className="header">
@@ -9,16 +36,24 @@ const ImageGenerator = () => {
       </div>
       <div className="img-loading">
         <div className="image">
-          <img src={default_image} alt=""></img>
+          <img src={image_url === "/" ? default_image : image_url} alt=""></img>
         </div>
       </div>
       <div className="search-box">
         <input
           type="text"
+          ref={inputRef}
           className="search-input"
           placeholder="Describe What You Want TO See"
         />
-        <div className="generate-btn">Generate</div>
+        <div
+          className="generate-btn"
+          onClick={() => {
+            imageGenerator();
+          }}
+        >
+          Generate
+        </div>
       </div>
     </div>
   );
